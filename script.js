1b54ef19b77a28d6bbeafad2272e7e04bf52da19
brandreplicas <80389637+brandreplicas@users.noreply.github.com>
2025-01-22 05:59:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@
     }, false);
 })();
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+  navigator.serviceWorker.register('service-worker.js').then(registration => {
     console.log('Service Worker registered with scope:', registration.scope);
   }).catch(error => {
     console.error('Service Worker registration failed:', error);
