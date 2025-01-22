1b61f30c6a84ed33a5cdabf1819e49be293cc669
brandreplicas <80389637+brandreplicas@users.noreply.github.com>
2025-01-22 05:40:09 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('site-cache').then(cache => {
+      return cache.addAll([
+        '/gallery.html',
+        '/index.html',
+        '/style.css',
+        '/css.css',
+        '/script.js',
+        '/fashluxee.jpg',
+        '/fashluxee-logo-transformed.png',
+        '/preview.jpg',
+        'https://urlsml.in/carrd-db/cors/category-list.php',
+        'https://urlsml.in/carrd-db/cors/media-list.php'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
