90818c7c1a20b4bf942a2b79ac43e9cb7346fef2
brandreplicas <80389637+brandreplicas@users.noreply.github.com>
2025-01-22 05:47:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,14 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('site-cache').then(cache => {
       return cache.addAll([
-        '/gallery.html',
-        '/index.html',
-        '/style.css',
-        '/css.css',
-        '/script.js',
-        '/fashluxee.jpg',
-        '/fashluxee-logo-transformed.png',
-        '/preview.jpg',
+        'gallery.html',
+        'index.html',
+        'style.css',
+        'css.css',
+        'script.js',
+        'fashluxee.jpg',
+        'fashluxee-logo-transformed.png',
+        'preview.jpg',
         'https://urlsml.in/carrd-db/cors/category-list.php',
         'https://urlsml.in/carrd-db/cors/media-list.php'
       ]);
