8de85bc61c0be03b91034fbd47e50e197ddddd27
brandreplicas <80389637+brandreplicas@users.noreply.github.com>
2025-04-11 09:08:10 +0530

    url changes

--- script.js
+++ script.js
@@ -13,7 +13,8 @@
     var noteEl = null;
     var resEl = null;
     var hadEl = null;
-    var datUrl = 'http://tecq.free.nf/cors';
+    // var datUrl = 'http://tecq.free.nf/cors';
+    var datUrl = 'https://urlsml.in/carrd-db/cors';
     var catUrl = datUrl + '/category-list.php';
     var medUrl = datUrl + '/media-list.php';
     var pos = 0;
